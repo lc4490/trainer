9e98f993e503d3d2e5768cbea590248c2c27ce08
Leo <lc4490@nyu.edu>
2024-12-07 20:03:43 -0500

    deleting unused profilepics

--- src/app/(dashboard)/equipment/page.tsx
+++ src/app/(dashboard)/equipment/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { api } from "../../../../convex/_generated/api";
-
 export default function HomePage() {
   return (
     <div className="flex-1 flex-col flex-divide-y">
